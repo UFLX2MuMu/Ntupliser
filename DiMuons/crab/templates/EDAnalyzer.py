b73742b9aac006eedc8c030cd307d08da501d193
Pierluigi Bortignon <bortigno@gmail.com>
2018-07-18 13:51:16 +0200

    For large production v2 we want to have systematics and full information

--- DiMuons/crab/templates/EDAnalyzer.py
+++ DiMuons/crab/templates/EDAnalyzer.py
@@ -124,10 +124,10 @@ process.dimuons = process.DiMuons.clone()
 # Overwrite the settings in the Ntupliser/DiMuons/python/UFDiMuonsAnalyzers*cff analyzers
 process.dimuons = process.DiMuons.clone()
 process.dimuons.isVerbose  = cms.untracked.bool(False)
-process.dimuons.doSys      = cms.bool(False)
+process.dimuons.doSys      = cms.bool(True)
 process.dimuons.doSys_KaMu = cms.bool(False)
-process.dimuons.doSys_Roch = cms.bool(False)
-process.dimuons.slimOut    = cms.bool(True) #reducing the number of branches. This should be the same in data and MC to avoid confusion.
+process.dimuons.doSys_Roch = cms.bool(True)
+process.dimuons.slimOut    = cms.bool(False) #reducing the number of branches. This should be the same in data and MC to avoid confusion.
 process.dimuons.skim_nMuons = cms.int32(2)
 
 
