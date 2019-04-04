9f1d4f46bf05235aeb8dce8f539ec6e5b9d7e8cf
Pierluigi Bortignon <bortigno@gmail.com>
2019-04-04 12:49:23 +0200

    updating crab script for 2018 production

--- DiMuons/crab/templates/crab_config.py
+++ DiMuons/crab/templates/crab_config.py
@@ -22,8 +22,7 @@ config.Data.inputDataset = 'STR'
 config.Data.useParent = False
 config.Data.splitting = 'STR'
 config.Data.unitsPerJob = NUM  ## Should take ~10 minutes for 100k events
-
-config.Data.outLFNDirBase = '/store/group/phys_higgs/HiggsExo/H2Mu/UF/ntuples/data_2017_and_mc_fall17/'
+config.Data.outLFNDirBase = '/store/group/phys_higgs/HiggsExo/H2Mu/UF/ntuples/2018/102X/'
 config.Data.publication = False
 config.Data.outputDatasetTag = 'STR'
 
