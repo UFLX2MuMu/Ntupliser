4d083b29acf62be88b72b61b317c44a525a6f538
Andrew Brinkerhoff <abrinke1@cern.ch>
2019-06-13 14:36:52 +0200

    Update hadd script

--- DiMuons/crab/hadd_files.sh
+++ DiMuons/crab/hadd_files.sh
@@ -2,41 +2,47 @@
 
 job_tag=$1
 
-eos_cmd="/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select"
-hadd_cmd="/cvmfs/cms.cern.ch/slc6_amd64_gcc530/cms/cmssw/CMSSW_8_0_25/external/slc6_amd64_gcc530/bin/hadd"
+hadd_cmd="/cvmfs/cms.cern.ch/slc6_amd64_gcc630/cms/cmssw/CMSSW_9_4_10/external/slc6_amd64_gcc630/bin/hadd"
 
-eos_pre="root://eoscms.cern.ch/"
-eos_dir="/store/group/phys_higgs/HiggsExo/H2Mu/UF/ntuples/Moriond17/"
+eos_dir="/eos/cms/store/group/phys_higgs/HiggsExo/H2Mu/UF/ntuples/2017/94X_v2/"
 tmp_dir="/afs/cern.ch/work/a/abrinke1/tmp"
-max_add=10
+max_add=5
 
 ## Clean up tmp directory
 `rm $tmp_dir/NTuple_*.root`
 `rm $tmp_dir/tuple_*.root`
 
-for dir1 in `$eos_cmd ls $eos_dir$job_tag`; do
-    for dir2 in `$eos_cmd ls $eos_dir$job_tag/$dir1`; do
+for dir1 in `ls $eos_dir$job_tag`; do
+    for dir2 in `ls $eos_dir$job_tag/$dir1`; do
 
-	## Remove existing hadded files
-	for dir3 in `$eos_cmd ls $eos_dir$job_tag/$dir1/$dir2`; do
+	# ## Only look at hiM
+	# if test "${dir2#*hiM_MG}" == "$dir2"; then
+	#     continue
+	# fi
+	# ## Skip SingleMu
+	# if test "${dir2#*SingleMu}" != "$dir2"; then
+	# 	continue
+	# fi
+	# ## Skip hiM
+	# if test "${dir2#*hiM}" != "$dir2"; then
+	# 	continue
+	# fi
+
+	## Remove existing hadded files for this MC sample
+	for dir3 in `ls $eos_dir$job_tag/$dir1/$dir2`; do
 	    if test "${dir3#*root}" != "$dir3"; then
 		echo "Removing existing $eos_dir$job_tag/$dir1/$dir2/$dir3"
-		`$eos_cmd rm $eos_dir$job_tag/$dir1/$dir2/$dir3`
+		# `rm $eos_dir$job_tag/$dir1/$dir2/$dir3`
 		continue
 	    fi
 	done
 
-	## Create new hadded files
-	for dir3 in `$eos_cmd ls $eos_dir$job_tag/$dir1/$dir2`; do
-
-	    # ## Only look at SingleMu_2016B
-	    # if test "${dir2#*SingleMu_2016B}" == "$dir2"; then
-	    # 	continue
-	    # fi
+	## Create new hadded files for this MC sample
+	for dir3 in `ls $eos_dir$job_tag/$dir1/$dir2`; do
 
 	    ## Find most recent crab subission
 	    last_ver=$dir3
-	    for dir3a in `$eos_cmd ls $eos_dir$job_tag/$dir1/$dir2`; do
+	    for dir3a in `ls $eos_dir$job_tag/$dir1/$dir2`; do
 		if test "${dir3a#*root}" == "$dir3a"; then
 		    last_ver=$dir3a
 		fi
@@ -51,10 +57,10 @@ for dir1 in `$eos_cmd ls $eos_dir$job_tag`; do
 		continue
 	    fi
 	    
-	    for dir4 in `$eos_cmd ls $eos_dir$job_tag/$dir1/$dir2/$dir3`; do
+	    for dir4 in `ls $eos_dir$job_tag/$dir1/$dir2/$dir3`; do
 
 		nFiles=0
-		for fName in `$eos_cmd ls $eos_dir$job_tag/$dir1/$dir2/$dir3/$dir4`; do
+		for fName in `ls $eos_dir$job_tag/$dir1/$dir2/$dir3/$dir4`; do
 		    if test "${fName#*.root}" != "$fName"; then
 			let "nFiles += 1"
 		    fi
@@ -71,13 +77,14 @@ for dir1 in `$eos_cmd ls $eos_dir$job_tag`; do
 
 		    hadd_str="$hadd_cmd $tmp_dir/NTuple_$nOut.root"
 		    nAdded=0
-		    for fName in `$eos_cmd ls $eos_dir$job_tag/$dir1/$dir2/$dir3/$dir4`; do
+		    for fName in `ls $eos_dir$job_tag/$dir1/$dir2/$dir3/$dir4`; do
 			if test "${used_files#*$fName}" != "$used_files"; then
 			    continue
 			fi
 			if test "${fName#*.root}" != "$fName"; then
 			    if [ "$nAdded" -lt $max_add ]; then
-				`xrdcp $eos_pre$eos_dir$job_tag/$dir1/$dir2/$dir3/$dir4/$fName $tmp_dir`
+				# echo "cp $eos_dir$job_tag/$dir1/$dir2/$dir3/$dir4/$fName $tmp_dir"
+				`cp $eos_dir$job_tag/$dir1/$dir2/$dir3/$dir4/$fName $tmp_dir`
 				hadd_str="$hadd_str $tmp_dir/$fName"
 				used_files="$used_files $fName"
 				let "nAdded += 1"
@@ -85,13 +92,16 @@ for dir1 in `$eos_cmd ls $eos_dir$job_tag`; do
 			    fi
 			fi
 		    done  ## Closes for loop over max_add files
+		    echo "About to hadd $eos_dir$job_tag/$dir1/$dir2"
+		    # echo "$hadd_str"
 		    `$hadd_str`
 		    echo ""
-		    echo "  * Wrote out $tmp_dir/NTuple_$nOut.root"
-		    echo ""
-		    `xrdcp $tmp_dir/NTuple_$nOut.root $eos_pre$eos_dir$job_tag/$dir1/$dir2`
+		    echo "  * Wrote out $tmp_dir/NTuple_$nOut.root ... "
+		    `cp $tmp_dir/NTuple_$nOut.root $eos_dir$job_tag/$dir1/$dir2`
 		    `rm $tmp_dir/NTuple_$nOut.root`
 		    `rm $tmp_dir/tuple_*.root`
+		    echo "    copied to $eos_dir$job_tag/$dir1/$dir2"
+		    echo ""
 		    let "nOut += 1"
 		done  ## Closes while loop over all files
 	    done  ## Closes for loop over dir4 ("0000")
