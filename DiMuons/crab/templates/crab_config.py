070463eab6336e173e2178a51b5068c1ab7dc2d7
Pierluigi Bortignon <bortigno@gmail.com>
2019-07-05 13:05:16 +0200

    change local eos store dir to bortigno

--- DiMuons/crab/templates/crab_config.py
+++ DiMuons/crab/templates/crab_config.py
@@ -23,7 +23,7 @@ config.Data.useParent = False
 config.Data.splitting = 'STR'
 config.Data.unitsPerJob = NUM  ## Should take ~10 minutes for 100k events
 
-config.Data.outLFNDirBase = '/store/group/phys_higgs/HiggsExo/H2Mu/X2MuMu/ntuples/2016/94X_v3/STR/'
+config.Data.outLFNDirBase = '/store/user/bortigno/h2mm/ntuples/2016/94X_v3/STR/'
 config.Data.publication = False
 config.Data.outputDatasetTag = 'STR'
 
